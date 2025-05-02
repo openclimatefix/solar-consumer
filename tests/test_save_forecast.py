30a11623b36398ffce8b72b704c007107aa306fe
peterdudfield <peter.dudfield@hotmail.com>
2025-05-02 17:54:12 +0100

    fix tests

--- tests/test_save_forecast.py
+++ tests/test_save_forecast.py
@@ -12,7 +12,7 @@ def test_save_generation_to_site_db(db_site_session):
     generation_data = {
         "target_datetime_utc": ["2023-10-01 00:00:00", "2023-10-01 01:00:00"],
         "solar_generation_kw": [100, 150],
-        "capacity_kw": [200, 201],
+        "capacity_kw": [20_000_000, 20_000_002],
     }
 
     # Convert to DataFrame
@@ -29,8 +29,9 @@ def test_save_generation_to_site_db(db_site_session):
 
     assert len(saved_data) == len(generation_df)
 
-    site = db_site_session.query(SiteSQL).first()
-    assert site.capacity_kw == 201
+    sites = db_site_session.query(SiteSQL).all()
+    assert len(sites) == 1
+    assert sites[0].capacity_kw == 20_000_002
 
 
 def test_save_forecasts_to_site_db(db_site_session):
