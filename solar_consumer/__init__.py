bf09d4bf32573bf978aa4e4484e46cffbb399d31
peterdudfield <peter.dudfield@hotmail.com>
2025-07-07 18:28:23 +0100

    get version for git tags

--- solar_consumer/__init__.py
+++ solar_consumer/__init__.py
@@ -1,2 +1,7 @@
 # neso_solar_consumer/__init__.py
-__version__ = "1.1.9"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("solar_consumer")
+except PackageNotFoundError:
+    __version__ = "v?"
