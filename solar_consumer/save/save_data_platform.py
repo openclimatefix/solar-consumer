dbcb34fddbe2784babd130389dd7bbee016fa50d
Peter Dudfield <peter.dudfield@hotmail.com>
2025-11-05 09:35:05 +0000

    get working for all gsps

--- solar_consumer/save/save_data_platform.py
+++ solar_consumer/save/save_data_platform.py
@@ -20,6 +20,8 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
     """
     Save Generation data to the Data-platform.
 
+    0. First we get all the locations
+
     Here's how we do it for each gsp
     1. Get only the data for that gsp
     2. Get the start and end timestamps from that data
@@ -44,11 +46,12 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
 
     gsp_ids = data_df["gsp_id"].unique()
 
-    # Here
+    # 0 Get all locations
+    all_locations = await get_all_gsp_and_national_locations(client)
 
     # for each gsp
     for gsp_id in gsp_ids:
-        logger.info(f"Processing GSP ID: {gsp_id}")
+        logger.info(f"Saving GSP ID: {gsp_id} to Data Platform")
 
         # 1. Get only the data for that gsp
         gsp_data = data_df[data_df["gsp_id"] == gsp_id]
@@ -58,22 +61,10 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
         end_timestamp_utc = gsp_data["target_datetime_utc"].max()
 
         # 3. Get the location for that gsp
-        # TODO get location, for GSPs also
-        if gsp_id == 0:
-            all_location_request = dp.ListLocationsRequest(
-                location_type_filter=dp.LocationType.NATION,
-                energy_source_filter=dp.EnergySource.SOLAR,
-            )
-            location_response = await client.list_locations(all_location_request)
-            location = location_response.locations[0]
-        else:
-            all_location_request = dp.ListLocationsRequest(
-                location_type_filter=dp.LocationType.GSP,
-                energy_source_filter=dp.EnergySource.SOLAR,
-            )
-            location_response = await client.list_locations(all_location_request)
-            location_response.locations[0]
-
+        location = all_locations.get(gsp_id)
+        if location is None:
+            logger.warning(f"No location found for GSP ID {gsp_id}, skipping.")
+            continue
         location_uuid = location.location_uuid
 
         # 4. Create an observer for that gsp and regime if it doesn't already exist
@@ -83,9 +74,7 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
         try:
             _ = await client.create_observer(observer_request)
         except Exception:
-            logger.warning(
-                f"Observer {name} probably already exists, so carrying on anyway."
-            )
+            logger.warning(f"Observer {name} probably already exists, so carrying on anyway.")
 
         # 5. Get the most recent observations for that location and observer
         recent_observations_request = dp.GetObservationsAsTimeseriesRequest(
@@ -102,21 +91,19 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
         )
 
         # 6. Remove any data points from our data that are already in the database
-        print(
+        logger.debug(
             f"Found {len(recent_observations.values)} recent observations for location {location_uuid} and observer {name}."
         )
         data_in_database = {
             obs.timestamp_utc: obs.value_fraction for obs in recent_observations.values
         }
-        gsp_data = gsp_data[
-            ~gsp_data["target_datetime_utc"].isin(data_in_database.keys())
-        ]
+        gsp_data = gsp_data[~gsp_data["target_datetime_utc"].isin(data_in_database.keys())]
 
         if len(gsp_data) == 0:
-            logger.info(
-                f"No new data to add for location {location_uuid} and observer {name}."
+            logger.debug(
+                f"No new data to add for location {location_uuid} and observer {name}, for gsp id {gsp_id}."
             )
-            return
+            continue
 
         # 7. Update location capacity based on the max capacity in this data
         new_max_capacity_watts = int(gsp_data["capacity_mwp"].max() * 1_000_000)
@@ -134,13 +121,16 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
             )
             _ = await client.update_location_capacity(update_location_request)
         else:
-            logger.info(
+            logger.debug(
                 f"Location {location_uuid} capacity of {max_capacity_watts_current}W is sufficient; no update needed."
             )
 
         # 8. Create new observations for the remaining data points
         observation_values = []
         for _, row in gsp_data.iterrows():
+            if row["capacity_mwp"] == 0:
+                continue  # Skip entries with zero capacity to avoid division by zero
+
             value_fraction = row["solar_generation_kw"] / (row["capacity_mwp"] * 1000)
             effective_capacity_watts = int(row["capacity_mwp"] * 1_000_000)
             timestamp_utc = row["target_datetime_utc"].to_pydatetime()
@@ -152,15 +142,41 @@ async def save_to_generation_to_data_platform(data_df: pd.DataFrame):
             )
             observation_values.append(oberservation_value)
 
-        logger.info(
+        logger.debug(
             f"Adding {len(observation_values)} new observation values for location {location_uuid} and observer {name}."
         )
-        observation_request = dp.CreateObservationsRequest(
-            location_uuid=location_uuid,
-            energy_source=dp.EnergySource.SOLAR,
-            observer_name=name,
-            values=observation_values,
-        )
-        _ = await client.create_observations(observation_request)
+        if len(observation_values) > 0:
+            observation_request = dp.CreateObservationsRequest(
+                location_uuid=location_uuid,
+                energy_source=dp.EnergySource.SOLAR,
+                observer_name=name,
+                values=observation_values,
+            )
+            _ = await client.create_observations(observation_request)
 
-    await channel.close()
+    channel.close()
+
+
+async def get_all_gsp_and_national_locations(
+    client: dp.DataPlatformDataServiceStub,
+) -> dict[int, dp.ListLocationsResponseLocationSummary]:
+    """Get all GSP and national locations for solar energy source"""
+
+    all_locations = {}
+    all_location_request = dp.ListLocationsRequest(
+        location_type_filter=dp.LocationType.NATION,
+        energy_source_filter=dp.EnergySource.SOLAR,
+    )
+    location_response = await client.list_locations(all_location_request)
+    location = location_response.locations[0]
+    all_locations = {0: location}
+
+    all_location_gsp_request = dp.ListLocationsRequest(
+        location_type_filter=dp.LocationType.GSP,
+        energy_source_filter=dp.EnergySource.SOLAR,
+    )
+    location_response = await client.list_locations(all_location_gsp_request)
+    for loc in location_response.locations:
+        all_locations[loc.metadata.to_dict()["gsp_id"]["numberValue"]] = loc
+
+    return all_locations
