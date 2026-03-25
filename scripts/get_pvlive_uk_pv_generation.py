49bef0a4d7c6e62a8c98814c9f346398276dfee6
James Fulton <41546094+dfulu@users.noreply.github.com>
2026-03-25 16:38:11 +0000

    Update script description

--- scripts/get_pvlive_uk_pv_generation.py
+++ scripts/get_pvlive_uk_pv_generation.py
@@ -3,7 +3,7 @@ the PVLive API for all GSPs in Great Britain.
 
 The downloaded data is saved as a Zarr dataset.
 
-This script takes around 40 mins to download 6 years of data for all GSPs.
+This script takes around 40 mins to download 7 years of data for all GSPs.
 """
 
 import fnmatch
