c3cd34ea0854b5b6c08657482fa8e032dbbda004
Peter Dudfield <peter.dudfield@hotmail.com>
2026-05-13 16:35:19 +0100

    move tests and mock

--- tests/unit/test_fetch_data_nl.py
+++ tests/unit/test_fetch_data_nl.py
@@ -6,6 +6,8 @@ from unittest.mock import patch, Mock
 import numpy as np
 import pandas as pd
 from solar_consumer.data.fetch_nl_data import fetch_nl_data, check_national_capacity_equals_regional_sum
+from solar_consumer.data.fetch_nl_data import get_entsoe_day_prices, make_potential_generation
+from unittest.mock import MagicMock
 
 
 @patch("solar_consumer.data.fetch_nl_data.requests.Session.get")
@@ -97,3 +99,64 @@ def test_check_national_capacity_equals_regional_sum():
     assert result["update_capacity"].loc[::3].all() # all capacities can be updated
     assert not result["update_capacity"].loc[1::3].any() # no capacities can be updated
     assert not result["update_capacity"].loc[2::3].any() # no capacities can be update
+
+
+
+
+@patch("solar_consumer.data.fetch_nl_data.EntsoePandasClient")
+def test_get_entsoe_day_prices(mock_entsoe_pandas_client):
+
+    start = pd.Timestamp('2026-05-12').tz_localize('UTC')
+    end = pd.Timestamp('2026-05-13').tz_localize('UTC')
+    api_key = ''
+
+    mock_da_prices = pd.DataFrame({
+        'target_datetime_utc': pd.date_range(start=start, end=end, freq='15min'),
+        'price': [10] * 97
+    })
+    mock_da_prices.set_index('target_datetime_utc', inplace=True)
+    mock_entsoe_pandas_client_instance = MagicMock()
+    mock_entsoe_pandas_client.return_value = mock_entsoe_pandas_client_instance
+    mock_entsoe_pandas_client_instance.query_day_ahead_prices.return_value = mock_da_prices
+
+    prices = get_entsoe_day_prices(start=start, end=end, api_key=api_key)
+    assert prices is not None
+    assert isinstance(prices, pd.DataFrame)
+    assert not prices.empty
+    assert len(prices) == 97 # 15 minute intervals in one day + 1
+
+
+@patch("solar_consumer.data.fetch_nl_data.EntsoePandasClient")
+def test_make_potential_generation(mock_entsoe_pandas_client):
+    start = pd.Timestamp('2026-05-10').tz_localize('UTC')
+    end = pd.Timestamp('2026-05-11').tz_localize('UTC')
+
+    data = pd.DataFrame({
+        'target_datetime_utc': pd.date_range(start=start, end=end, freq='15T'),
+        'solar_generation_kw': [1] * 97
+    })
+
+    mock_da_prices = pd.DataFrame({
+        'target_datetime_utc': pd.date_range(start=start, end=end, freq='15min'),
+        'price': [10] * 33 + [-1] * 25 + [1]* 39
+    })
+    mock_da_prices.set_index('target_datetime_utc', inplace=True)
+    mock_entsoe_pandas_client_instance = MagicMock()
+    mock_entsoe_pandas_client.return_value = mock_entsoe_pandas_client_instance
+    mock_entsoe_pandas_client_instance.query_day_ahead_prices.return_value = mock_da_prices
+
+    potential_generation = make_potential_generation(data=data)
+    assert potential_generation is not None
+    assert isinstance(potential_generation, pd.DataFrame)
+    assert not potential_generation.empty
+    assert len(potential_generation) == 97 # 15 minute intervals in one day + 1
+    assert potential_generation.iloc[0].solar_generation_no_curtailment_kw == 1 # 00:00 is 1
+    assert potential_generation.iloc[48].solar_generation_no_curtailment_kw == 1.11 #12:00 has been increased
+
+    # we know that the prices at 8:15 to 14:15 was negative
+    # lets check these values
+    negative_prices_idx = (potential_generation['target_datetime_utc'].dt.time >= pd.Timestamp('08:15').time()) \
+        & (data['target_datetime_utc'].dt.time <= pd.Timestamp('14:15').time())
+    # check the negative prices are all zero
+    assert (potential_generation.loc[negative_prices_idx, "solar_generation_no_curtailment_kw"]== 1.11).all()
+    assert (potential_generation.loc[~negative_prices_idx, "solar_generation_no_curtailment_kw"]== 1).all()
